3c8011a175d3bc54dcbc9b65c33cdbd4bf2f24c8
Ilya Kolpakov <ilya.kolpakov@gmail.com>
2018-05-31 16:33:26 +0200

    fix clang in env

--- shell.nix
+++ shell.nix
@@ -4,13 +4,19 @@ stdenv.mkDerivation {
   name = "gcc-env";
   libraries = [
   ];
-  buildInputs = [
-    pkgs.gcc
-    pkgs.gdb
-    pkgs.valgrind
-    pkgs.gnumake
-    pkgs.zlib
-    pkgs.clang_6
+  buildInputs = with pkgs; [
+    gcc
+    gdb
+    valgrind
+    gnumake
+    zlib
+    clang_6
     #pkgs.pthreads
   ];
+
+  # prevent clang package from interfering with options
+  # https://github.com/NixOS/nixpkgs/issues/29877
+  shellHook = ''
+      export NIX_CXXSTDLIB_LINK=""
+  '';
 }
