d9f9380626cbc289ed4d0104f7f7c5cfb9b1dc49
Ilya Kolpakov <ilya.kolpakov@gmail.com>
2018-06-03 17:40:16 +0200

    remove obsolete code in include/memory.hpp

--- include/memory.hpp
+++ include/memory.hpp
@@ -9,22 +9,4 @@ struct no_deleter {
     void operator()(T* p){}
 };
 
-
-namespace impl {
-
-//! Defines a deleter struct compaitble with std::unique_ptr
-template<typename T, void (*f)(T* p)>
-struct custom_deleter {
-	inline void operator()(T* p){ f(p); }
-};
-
-}
-
-template<typename T>
-void noop_deleter(T* p){}
-
-// Untyped producers/consumers wrapped via std::unique_ptr
-template<typename T, void (*f)(T* p)>
-using unique_ptr = std::unique_ptr<T, impl::custom_deleter<T, f>>;
-
 }
